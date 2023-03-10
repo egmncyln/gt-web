d2b27c58b8acbe6ea888d06f00f945334a3d999f
Egemen Ceylan <egemen.ceylan1@gmail.com>
2023-03-10 21:53:56 +0300

    first basic lit web component is created

---
+++ src/components/ui/text/mobile-text.js
@@ -0,0 +1,8 @@
+import { LitElement, html } from 'lit';
+
+class MobileText extends LitElement {
+  render() {
+    return html`<p><slot><slot></p>`;
+  }
+}
+customElements.define('mobile-text', MobileText);
